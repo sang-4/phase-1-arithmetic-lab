81513c83f9b4e8a8ff33f58c4a6a3313a67e4acd
Allan <kipropsallan@gmail.com>
2022-06-10 15:04:13 +0300

    commit tree d51e6b5533ce0a1f60536f1e9441f269fd56e529

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31,
+    num2=2,
+    num3 = 31,
+    num4 = 9;
 
-// Write your code here
+    
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*10)+1;
+
+let mod = num3%num4;
+
+let max = Math.max(1,2,3,4,5,6,6,17,3,20);
